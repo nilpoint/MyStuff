9afc26e76870836a6c085aa0c72119304cb1d19a
Nil Point <nilpoint@hotmail.com>
2015-09-02 17:08:03 +0800

    Initial Commit

---
+++ MyStuff/DetailViewController.swift
@@ -0,0 +1,45 @@
+//
+//  DetailViewController.swift
+//  MyStuff
+//
+//  Created by John Alstru on 9/2/15.
+//  Copyright (c) 2015 nilpoint.sample. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+  @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+  var detailItem: AnyObject? {
+    didSet {
+        // Update the view.
+        self.configureView()
+    }
+  }
+
+  func configureView() {
+    // Update the user interface for the detail item.
+    if let detail: AnyObject = self.detailItem {
+        if let label = self.detailDescriptionLabel {
+            label.text = detail.description
+        }
+    }
+  }
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    self.configureView()
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
