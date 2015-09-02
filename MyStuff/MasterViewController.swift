2042fd8600891fd86be1bdbab5cc33a07da94475
Nil Point <nilpoint@hotmail.com>
2015-09-02 18:35:40 +0800

    Display a stuff list.

--- MyStuff/MasterViewController.swift
+++ MyStuff/MasterViewController.swift
@@ -11,8 +11,13 @@ import UIKit
 class MasterViewController: UITableViewController {
 
   var detailViewController: DetailViewController? = nil
-  var objects = [AnyObject]()
-
+  var things = [
+    MyWhatsit(name: "Gort", location: "den"),
+    MyWhatsit(name: "Disappearing TARDIS mug", location: "kitchen"),
+    MyWhatsit(name: "Robot USB drive", location: "office"),
+    MyWhatsit(name: "Sad Robot USB hub", location: "office"),
+    MyWhatsit(name: "Solar Powered Bunny", location: "office")
+  ]
 
   override func awakeFromNib() {
     super.awakeFromNib()
@@ -41,23 +46,23 @@ class MasterViewController: UITableViewController {
   }
 
   func insertNewObject(sender: AnyObject) {
-    objects.insert(NSDate(), atIndex: 0)
-    let indexPath = NSIndexPath(forRow: 0, inSection: 0)
-    self.tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
+//    things.insert(NSDate(), atIndex: 0)
+//    let indexPath = NSIndexPath(forRow: 0, inSection: 0)
+//    self.tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
   }
 
   // MARK: - Segues
 
   override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-    if segue.identifier == "showDetail" {
-        if let indexPath = self.tableView.indexPathForSelectedRow() {
-            let object = objects[indexPath.row] as! NSDate
-            let controller = (segue.destinationViewController as! UINavigationController).topViewController as! DetailViewController
-            controller.detailItem = object
-            controller.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem()
-            controller.navigationItem.leftItemsSupplementBackButton = true
-        }
-    }
+//    if segue.identifier == "showDetail" {
+//        if let indexPath = self.tableView.indexPathForSelectedRow() {
+//            let object = things[indexPath.row] as! NSDate
+//            let controller = (segue.destinationViewController as! UINavigationController).topViewController as! DetailViewController
+//            controller.detailItem = object
+//            controller.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem()
+//            controller.navigationItem.leftItemsSupplementBackButton = true
+//        }
+//    }
   }
 
   // MARK: - Table View
@@ -67,14 +72,17 @@ class MasterViewController: UITableViewController {
   }
 
   override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return objects.count
+    return things.count
   }
 
+  // This is your rubber stamp. 
+  // This function is called every time the table view wants to draw a row.
   override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as! UITableViewCell
 
-    let object = objects[indexPath.row] as! NSDate
-    cell.textLabel!.text = object.description
+    let thing = things[indexPath.row] as MyWhatsit
+    cell.textLabel?.text = thing.name
+    cell.detailTextLabel?.text = thing.location
     return cell
   }
 
@@ -85,7 +93,7 @@ class MasterViewController: UITableViewController {
 
   override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
     if editingStyle == .Delete {
-        objects.removeAtIndex(indexPath.row)
+        things.removeAtIndex(indexPath.row)
         tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
     } else if editingStyle == .Insert {
         // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
