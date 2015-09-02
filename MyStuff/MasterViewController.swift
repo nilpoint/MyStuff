927390e5de16b2821ec96c72883c5634f281d10c
Nil Point <nilpoint@hotmail.com>
2015-09-02 20:53:21 +0800

    Display the Whatsit info in the detail view.

--- MyStuff/MasterViewController.swift
+++ MyStuff/MasterViewController.swift
@@ -54,15 +54,15 @@ class MasterViewController: UITableViewController {
   // MARK: - Segues
 
   override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-//    if segue.identifier == "showDetail" {
-//        if let indexPath = self.tableView.indexPathForSelectedRow() {
-//            let object = things[indexPath.row] as! NSDate
-//            let controller = (segue.destinationViewController as! UINavigationController).topViewController as! DetailViewController
-//            controller.detailItem = object
-//            controller.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem()
-//            controller.navigationItem.leftItemsSupplementBackButton = true
-//        }
-//    }
+    if segue.identifier == "showDetail" {
+        if let indexPath = self.tableView.indexPathForSelectedRow() {
+            let thing = things[indexPath.row]
+            let controller = (segue.destinationViewController as! UINavigationController).topViewController as! DetailViewController
+            controller.detailItem = thing
+            controller.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem()
+            controller.navigationItem.leftItemsSupplementBackButton = true
+        }
+    }
   }
 
   // MARK: - Table View
