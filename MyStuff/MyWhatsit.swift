3066fd4b6d90117f9d007bce633a4190890f0044
Nil Point <nilpoint@hotmail.com>
2015-09-02 17:24:13 +0800

    Add the class MyWhatsit as a data model.

---
+++ MyStuff/MyWhatsit.swift
@@ -0,0 +1,19 @@
+//
+//  MyWhatsit.swift
+//  MyStuff
+//
+//  Created by John Alstru on 9/2/15.
+//  Copyright (c) 2015 nilpoint.sample. All rights reserved.
+//
+
+import Foundation
+
+class MyWhatsit {
+  var name: String
+  var location: String
+  
+  init(name: String, location: String = "") {
+    self.name = name
+    self.location = location
+  }
+}
