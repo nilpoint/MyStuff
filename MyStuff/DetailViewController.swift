16f00f9c27a6c43606b89c8ffeae0bfaa2e85b02
Nil Point <nilpoint@hotmail.com>
2015-09-03 11:15:44 +0800

    Get a picture and display it in the detail view.

--- MyStuff/DetailViewController.swift
+++ MyStuff/DetailViewController.swift
@@ -7,15 +7,57 @@
 //
 
 import UIKit
+import MobileCoreServices
 
-class DetailViewController: UIViewController {
+class DetailViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
   @IBOutlet var nameField: UITextField!
   @IBOutlet var locationField: UITextField!
   @IBOutlet var imageView: UIImageView!
   
   @IBAction func choosePicture(_: AnyObject!) {
+    if detailItem == nil {
+      return
+    }
     
+    let hasPhotos = UIImagePickerController.isSourceTypeAvailable(.PhotoLibrary)
+    let hasCamera = UIImagePickerController.isSourceTypeAvailable(.Camera)
+    
+    switch (hasPhotos, hasCamera) {
+    case (true, true):
+      let alert = UIAlertController(title: nil, message: nil, preferredStyle: .ActionSheet)
+      alert.addAction(UIAlertAction(
+        title: "Take a Picture",
+        style: .Default,
+        handler: {(_) in
+          self.presentImagePicker(.Camera)
+      }))
+      alert.addAction(UIAlertAction(
+        title: "Choose a Photo",
+        style: .Default,
+        handler: {(_) in
+          self.presentImagePicker(.PhotoLibrary)
+      }))
+      alert.addAction(UIAlertAction(
+        title: "Cancel",
+        style: .Cancel,
+        handler: nil))
+      if let popover = alert.popoverPresentationController {
+        popover.sourceView = imageView
+        popover.sourceRect = imageView.bounds
+        popover.permittedArrowDirections = ( .Up | .Down)
+      }
+      presentViewController(alert, animated: true, completion: nil)
+      break
+    case (true, false):
+      presentImagePicker(.PhotoLibrary)
+      break
+    case (false, true):
+      presentImagePicker(.Camera)
+      break
+    default:
+      break
+    }
   }
 
   var detailItem: MyWhatsit? {
@@ -53,6 +95,29 @@ class DetailViewController: UIViewController {
     } else if sender === locationField {
       detailItem?.location = locationField.text
     }
+  }
+  
+  func presentImagePicker(source: UIImagePickerControllerSourceType) {
+    let imagePicker = UIImagePickerController()
+    imagePicker.sourceType = source
+    imagePicker.mediaTypes = [kUTTypeImage as String]
+    imagePicker.delegate = self
+    presentViewController(imagePicker, animated: true, completion: nil)
+  }
+  
+  func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
+    var image: UIImage! = info[UIImagePickerControllerEditedImage] as? UIImage
+    if image == nil {
+      image = info[UIImagePickerControllerOriginalImage] as! UIImage
+    }
+    
+    if picker.sourceType == .Camera {
+      UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+    }
+    
+    detailItem?.image = image
+    imageView.image = image
+    dismissViewControllerAnimated(true, completion: nil)
   }
 
 }
